9f65630d00f431b7181e13f7cceebb05ea432329
Sercan <sercangedik@gmail.com>
2017-01-19 23:51:39 +0300

    scale and rotate

--- Assets/CubeColoring.cs
+++ Assets/CubeColoring.cs
@@ -6,7 +6,7 @@ public class CubeColoring : MonoBehaviour {
 	private Matrix processMatrix = new Matrix(4, 4);
 	private Matrix objectMatrix = new Matrix(4, 1);
 	private Matrix result = new Matrix (4, 1);
-	private Vector3 vector = new Vector3 (0, 1, 0);
+	private Vector3 vector = new Vector3 (0, 0, 0);
 	private Vector3 move = new Vector3 (0, 0 ,0);
 
 	// Use this for initialization
@@ -16,23 +16,21 @@ public class CubeColoring : MonoBehaviour {
 		objectMatrix [2, 0] = transform.position.z;
 		objectMatrix [3, 0] = 1;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		vector.x  = vector.x + Time.deltaTime / 2000;
-		vector.y  = vector.y + Time.deltaTime / 2000;
-		vector.z  = vector.z + Time.deltaTime / 2000;
-		getRotatingMatrixForZ (Time.deltaTime);
 
-		Debug.Log ("Vector - X : " + (float)vector.x + " Y : " + vector.y + " Z : " + vector.z);
+		//vector.x  = vector.x + Time.time / 2000;
+		vector.y  = vector.y + Time.time / 2000;
+		//vector.z  = vector.z + Time.time / 2000;
+		getMovingMatrix (vector);
+
 		result = NaiveMultiplication (processMatrix, objectMatrix);
 		move.x = (float)result [0, 0]; 
 		move.y = (float)result [1, 0];
 		move.z = (float)result [2, 0];
 
-		transform.position += move;
-
-		Debug.Log ("Transform - X : " + (float)result [0, 0] + " Y : " + (float)result [1, 0] + " Z : " + (float)result [2, 0]);
+		transform.position = move;
 	}
 
 	void getMovingMatrix(Vector3 vector) {
@@ -46,6 +44,24 @@ public class CubeColoring : MonoBehaviour {
 					processMatrix [i, j] = vector.y;
 				} else if (i == 2 && j == 3) {
 					processMatrix [i, j] = vector.z;
+				} else {
+					processMatrix [i, j] = 0;
+				}
+			}
+		}
+	}
+
+	void getScalingMatrix(Vector3 vector) {
+		for(int i=0; i < processMatrix.Height; ++i){
+			for(int j=0; j < processMatrix.Width; ++j){
+				if (i == 0 & j == 0) {
+					processMatrix [i, j] = vector.x;
+				} else if (i == 1 & j == 1) {
+					processMatrix [i, j] = vector.y;
+				} else if (i == 2 & j == 2) {
+					processMatrix [i, j] = vector.z;
+				} else if (i == 3 && j == 3) {
+					processMatrix [i, j] = 1;
 				} else {
 					processMatrix [i, j] = 0;
 				}
@@ -112,7 +128,7 @@ public class CubeColoring : MonoBehaviour {
 			}
 		}
 	}
-		
+
 	Matrix NaiveMultiplication(Matrix m1, Matrix m2)
 	{
 		Matrix resultMatrix = new Matrix(m1.Height, m2.Width);
